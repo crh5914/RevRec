82f6d31b7586528319b809a1e37d0728b3de09e8
crh5914 <249683003@qq.com>
2018-11-16 18:13:11 +0800

    code

---
+++ train.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+from gensim.models import Word2Vec
+from util import *
+from simple_model import SimpleModel
+from tqdm import tqdm
+def main():
+    prefix = './{}/{}'.format('baby','baby')
+    dim = 100
+    epochs = 50
+    layer_size = [64,32,16,1]
+    lr = 0.0001
+    alpha = 0.5
+    num_users,num_items = get_user_item_info(prefix+'_umap.csv',prefix+'_itmap.csv')
+    train_users,train_items,train_revs,train_rs,word_dict,maxlen = load_train_data(prefix+'_train.csv')
+    test_users,test_items,test_rs = load_test_data(prefix+'_test.csv')
+    vocab = get_vocab(word_dict)
+    w2v = Word2Vec.load(prefix+'_w2v.emb')
+    word_emb = get_word_emb(vocab,dim,w2v)
+    sess = tf.Session()
+    model = SimpleModel(sess,num_users,num_items,maxlen,len(vocab),dim,layer_size,lr,alpha)
+    sess.run(model.word_emb.assign(tf.constant(word_emb,dtype=tf.float32)))
+    best_mse,best_epoch = 10,0
+    for epoch in range(epochs):
+        loss,mse = 0,0
+        for batch_u,batch_i,batch_rev,batch_len,batch_r in tqdm(generate_train_batch(train_users,train_items,train_revs,train_rs,vocab,maxlen)):
+            batch_mse,batch_loss = model.train(batch_u,batch_i,batch_rev,batch_len,batch_r)
+            loss += batch_loss*len(batch_u)
+            mse += batch_mse*len(batch_u)
+        mse = mse / len(train_users)
+        loss = loss / len(train_users)
+        print('train epoch:{},mse:{},loss:{}'.format(epoch+1,mse,loss))
+        mse = 0
+        for batch_u,batch_i,batch_r in tqdm(generate_test_batch(test_users,test_items,test_rs)):
+            y,batch_mse = model.test(batch_u,batch_i,batch_r)
+            mse += len(batch_u) * batch_mse
+        mse = mse / len(test_users)
+        if mse < best_mse:
+            best_mse = mse
+            best_epoch = epoch + 1
+        print('test epoch:{},mse:{}'.format(epoch+1,mse))
+    print('best mse:{},at epoch:{}'.format(best_mse,best_epoch))
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,78 @@
+import numpy as np
+from sklearn.utils import shuffle
+def get_word_emb(vocab,dim,model):
+    emb_matrix = np.zeros(shape=(len(vocab),dim))
+    for word in vocab:
+        if word in model:
+            emb_matrix[vocab[word]] = model[word] 
+    return emb_matrix
+
+def load_train_data(file):
+    users,items,revs,rs = [],[],[],[]
+    vocab = {}
+    maxlen = 0
+    with open(file,'r') as fp:
+         for line in fp:
+             line = line.strip()
+             vals = line.split(',')
+             users.append(int(vals[0])-1)
+             items.append(int(vals[1])-1)
+             words = vals[2].split()
+             revs.append(words)
+             if len(words) > maxlen:
+                 maxlen = len(words)
+             for word in words:
+                 vocab[word] = vocab.get(word,0) + 1
+             rs.append(float(vals[3]))
+    return users,items,revs,rs,vocab,maxlen
+def load_test_data(file):
+    users,items,rs = [],[],[]
+    with open(file,'r') as fp:
+         for line in fp:
+             line = line.strip()
+             vals = line.split(',')
+             users.append(int(vals[0])-1)
+             items.append(int(vals[1])-1)
+             rs.append(float(vals[3]))
+    return users,items,rs
+
+def generate_test_batch(users,items,rs,batch_size=256):
+    batch_u,batch_i,batch_r = [],[],[]
+    for u,i,r in zip(users,items,rs):
+        batch_u.append(u)
+        batch_i.append(i)
+        batch_r.append(r)
+        if len(batch_u) == batch_size:
+            yield batch_u,batch_i,batch_r
+            batch_u,batch_i,batch_r = [],[],[]
+    if len(batch_u) > 0:
+        yield batch_u,batch_i,batch_r
+
+def get_user_item_info(ufile,ifile):
+    with open(ufile,'r') as fp:
+        num_users = len(fp.readlines())
+    with open(ifile,'r') as fp:
+        num_items = len(fp.readlines())
+    return num_users,num_items
+
+def get_vocab(words_count):
+    vocab = {word:idx+1 for idx,word in enumerate(words_count)}
+    return vocab
+
+def generate_train_batch(users,items,revs,rs,vocab,maxlen,batch_size=256):
+    users,items,revs,rs = shuffle(users,items,revs,rs)
+    batch_u,batch_i,batch_rev,batch_len,batch_r = [],[],[],[],[]
+    for u,i,rev,r in zip(users,items,revs,rs):
+        batch_u.append(u)
+        batch_i.append(i)
+        rev = [ vocab[word] for word in rev]
+        batch_len.append([len(rev)])
+        rev = rev + [0]*(maxlen-len(rev)) 
+        batch_rev.append(rev)
+        batch_r.append(r)
+        if len(batch_u) == batch_size:
+            yield batch_u,batch_i,batch_rev,batch_len,batch_r
+            batch_u,batch_i,batch_rev,batch_len,batch_r = [],[],[],[],[]
+    if len(batch_u) > 0:
+        yield batch_u,batch_i,batch_rev,batch_len,batch_r
+        
